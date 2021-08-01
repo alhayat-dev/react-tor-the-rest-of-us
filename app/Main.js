6c108ef35c537df80a7d3ed9926c338e453ad643
alhayat-dev <alhayat.dev@gmail.com>
2021-08-01 07:01:38 +0500

    Conditionally display the homepage whether the user is logged in or not using traditonal way by using useState and uplift it in the top level components.

--- app/Main.js
+++ app/Main.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import ReactDom from "react-dom"
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 
@@ -9,14 +9,17 @@ import Footer from "./components/Footer"
 import HomeGuest from "./components/HomeGuest"
 import About from "./components/About"
 import Terms from "./components/Terms"
+import Home from "./components/Home"
 
 function Main() {
+  const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem("complexappToken")))
+
   return (
     <BrowserRouter>
-      <Header />
+      <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
       <Switch>
         <Route path="/" exact>
-          <HomeGuest />
+          {loggedIn ? <Home /> : <HomeGuest />}
         </Route>
         <Route path="/about-us">
           <About />
